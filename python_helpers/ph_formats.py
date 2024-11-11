192a1c0e082f89e836f34ca6a3dc54462cd7e7e7
imPratik <impratikjaiswal@gmail.com>
2024-11-11 07:40:24 +0530

    phFormats added

---
+++ python_helpers/ph_formats.py
@@ -0,0 +1,36 @@
+class PhFormats:
+    TXT = 'txt'
+    ASN = 'asn'
+    ASN1 = 'asn1'
+    BASE_64 = 'base64'
+    HEX = 'hex'
+    YML = 'yml'
+    YAML = 'yaml'
+    JSON = 'json'
+    IN = 'in'
+    OUT = 'out'
+    CSV = 'csv'
+    XLSX = 'xlsx'
+    XLS = 'xls'
+    PNG = 'png'
+    SVG = 'svg'
+    JPG = 'jpg'
+    JPEG = 'jpeg'
+    BKP = 'bkp'
+    TMP = 'tmp'
+    PY = 'py'
+    JAVA = 'java'
+    _7Z = '7z'
+    ZIP = 'zip'
+    HASH = 'hash'
+    FUNCTION_SPECIFIC = 'function_specific'
+    ASN_ORG = 'asn_org'
+    ASN_MANUALLY_CORRECTED = 'asn_manually_corrected'
+    ASN_AUTO_CORRECTED = 'asn_auto_corrected'
+    ASN_FORMATTED = 'asn_formatted'
+
+
+class PhFormatsGroups:
+    ARCHIVE_OUTPUT_FORMATS_SUPPORTED = [
+        PhFormats.ZIP,
+    ]
